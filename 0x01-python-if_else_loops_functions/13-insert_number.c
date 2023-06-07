4a1bf5ccc79c7f3fe180cb99826fcb7802039a06
baffoechristian <christianbaffoe444@gmail.com>
2023-06-06 17:13:35 -0700

    5th task

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,36 +1,49 @@
 #include "lists.h"
 
 /**
- * insert_node - Inserts a new node at a given position.
- * @head: Pointer to the head of the list.
- * @number: Value to be inserted.
- *
- * Return: Address of the new node, or NULL if it failed.
+ * insert_node - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @number: index of the list where the new node is
+ * added.
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *current;
+	listint_t *new;
+	listint_t *h;
+	listint_t *h_prev;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	h = *head;
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = number;
-
-	if (*head == NULL || (*head)->n >= number)
+	while (h != NULL)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		if (h->n > number)
+			break;
+		h_prev = h;
+		h = h->next;
 	}
 
-	current = *head;
+	new->n = number;
 
-	while (current->next != NULL && current->next->n < number)
-		current = current->next;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = h;
+		if (h == *head)
+			*head = new;
+		else
+			h_prev->next = new;
+	}
 
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
+	return (new);
 }
