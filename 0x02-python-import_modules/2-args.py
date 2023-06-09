be41bdd00800367c1963d40619f3594940d936ca
baffoechristian <christianbaffoe444@gmail.com>
2023-06-08 17:28:54 -0700

    hey

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,25 @@
-!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
+#!/usr/bin/python3
+from sys import argv
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+
+def main():
+    length = len(argv) - 1
+    word = "arguments"
+
+    if length == 0:
+        print(f"{length} {word}.")
+    elif length != 1:
+        print(f"{length} {word}:")
     else:
-        print("{} arguments:".format(i))
+        word = "argument"
+        print(f"{length} {word}:")
 
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+    if length >= 1:
+        for count, args in enumerate(argv):
+            if count == 0:
+                continue
+            print(f"{count}: {args}")
+
+
+if __name__ == '__main__':
+    main()
